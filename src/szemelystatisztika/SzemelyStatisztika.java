e6a95fb162823466d3583bba3cc191dcfd3141af
kovacs.kevin <kovacs.kevin@DESKTOP-6R31BP8.akk.lan>
2026-09-10 15:48:33 +0200

    proceduralis megoldas

--- src/szemelystatisztika/SzemelyStatisztika.java
+++ src/szemelystatisztika/SzemelyStatisztika.java
@@ -7,6 +7,7 @@ package szemelystatisztika;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Random;
 
 /**
  *
@@ -19,18 +20,19 @@ public class SzemelyStatisztika {
      */
     static String nev = "Józsi";
     static int szulEv = 1998;
+    static Random rnd = new Random();
 
     public static void main(String[] args) throws IOException {
         hibak();
-        kiiras(koszontes(nev));
+        kiiras(koszontes(nev,5));
         kiiras("Születési éve: ", szulEv);
         int egyadottevben = evszamolas(szulEv, 2026);
         kiiras("2026 ban ennyi éves lesz: ", egyadottevben);
         kiiras("Nyugdíjáig hátrelévő év: ", nyugdij(egyadottevben, 65));
     }
-    
-    public static void hibak() throws IOException{
-        if(!marnyugdijas(szulEv)){
+
+    public static void hibak() throws IOException {
+        if (!marnyugdijas(szulEv)) {
             throw new IOException("Már nyugdíjas");
         }
     }
@@ -44,7 +46,22 @@ public class SzemelyStatisztika {
     }
 
     public static String koszontes(String nev) {
+
         return "Üdvözöllek %s!".formatted(nev);
+    }
+
+    public static String koszontes(String nev,int felsorange) {
+        int szam = rnd.nextInt(0, felsorange);
+        if (szam == 0) {
+            return "Üdvözöllek %s!".formatted(nev);
+        }
+        if (szam == 1) {
+            return "CSÓÓÓÓÓKOLLAK %s!".formatted(nev);
+        }
+        if (szam == 2) {
+            return "Legyél szerencsés szép %s öcsém!".formatted(nev);
+        }
+        return "Szia %s ! Bocsi de ennyi köszönés volt raktáron".formatted(nev);
     }
 
     public static int evszamolas(int szulEv, int targetEv) {
@@ -58,8 +75,9 @@ public class SzemelyStatisztika {
     public static int nyugdij(int adat, int nyugdij) {
         return nyugdij - evszamolas(adat);
     }
-    static boolean marnyugdijas(int nyugdij){
-        return nyugdij>0;
+
+    static boolean marnyugdijas(int nyugdij) {
+        return nyugdij > 0;
     }
 
 }
